2af0a027eda687038c51c7caf86410bbd7b0a83d
SeohyunPark <ttol6354@gmail.com>
2023-08-28 17:27:59 +0900

    스프링 업로드

---
+++ spring01_빈생성/src/main/java/UserMain.java
@@ -0,0 +1,37 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+import com.service.UserService;
+
+public class UserMain {
+
+	public static void main(String[] args) {
+		
+		//1. 이전 방식
+//		UserService service = new UserService();
+//		System.out.println(service.mesg());
+		
+		//2. 스프링 방식
+		ApplicationContext ctx = 
+				new GenericXmlApplicationContext("user.xml");
+		
+		
+		/*
+		 - 작업 순서
+    1) 빈을 생성( *.java)
+	2) 빈을 xml에 등록 ==> src/main/resource에 저장. user.xml
+	3) ApplicatonContext(빈) 에 xml을 알려준다.
+	4) ApplicatonContext 가 xml에 등록된 빈 정보를 보고 자동생성 ~ 소멸 처리함.
+		 
+		 */
+		
+		
+		
+		
+	}
+}
+
+
+
+
+
---
+++ spring01_빈생성3_multi/src/main/java/UserMain.java
@@ -0,0 +1,39 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+import com.service.UserService;
+
+public class UserMain {
+
+	public static void main(String[] args) {
+		
+		//1. 이전 방식
+//		UserService service = new UserService();
+//		System.out.println(service.mesg());
+		
+		//2. 스프링 방식
+//		ApplicationContext ctx = 
+//new GenericXmlApplicationContext("com/config/user.xml", "com/config/user2.xml");
+		ApplicationContext ctx = 
+new GenericXmlApplicationContext("com/config/*.xml"); // 가독성은 떨어짐.
+		
+		
+		/*
+		 - 작업 순서
+    1) 빈을 생성( *.java)
+	2) 빈을 xml에 등록 ==> src/main/resource에 저장. user.xml
+	3) ApplicatonContext(빈) 에 xml을 알려준다.
+	4) ApplicatonContext 가 xml에 등록된 빈 정보를 보고 자동생성 ~ 소멸 처리함.
+		 
+		 */
+		
+		
+		
+		
+	}
+}
+
+
+
+
+
